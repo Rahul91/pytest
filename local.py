48ad8717b8e821e5b35208f7adf0f86221ac5a25
Rahul Mishra <priyrahulmishra@gmail.com>
2015-04-04 22:56:07 +0530

    committing

---
+++ local.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+def change(b):
+    a = 90
+    print a
+a = 9
+print "Before the function call ", a
+print "inside change function",
+change(a)
+print "After the function call ", a
